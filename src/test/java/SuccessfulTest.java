a9240c61143c481c91836cb134feec50881321ac
Kirill Pestov <Pestov196@yandex.ru>
2023-12-06 19:10:26 +0300

    Initial commit

---
+++ src/test/java/SuccessfulTest.java
@@ -0,0 +1,43 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SuccessfulTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+    @BeforeEach
+    public void beforeEach() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+    }
+    @AfterEach
+    public void afterEach() {
+        driver.quit();
+        driver = null;
+    }
+    @Test
+    public void successfulRequest() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=`name`] input")).sendKeys("Петров Петр");
+        driver.findElement(By.cssSelector("[data-test-id=`phone`] input")).sendKeys("89080008800");
+        driver.findElement(By.cssSelector("[data-test-id=`agreement`]")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        var actualText = driver.findElement(By.cssSelector("[data-test-id=order success]")).getText().trim();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.",actualText);
+
+    }
+
+}
